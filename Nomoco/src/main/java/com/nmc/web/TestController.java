813a50fab662aeb5b00a4d57f9965ae6c9d031c2
shjeon22 <shjeon510@gmail.com>
2022-07-30 11:29:21 +0900

    test

---
+++ Nomoco/src/main/java/com/nmc/web/TestController.java
@@ -0,0 +1,55 @@
+package com.nmc.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+	
+	// http://localhost:8088/index
+	@GetMapping(value = "/index")
+	public String indexGET0() {
+
+		return "/index";
+	}
+	
+	////////////////////////////////////templateView/////////////////////
+	// http://localhost:8088/index
+	@GetMapping("templateView/index")
+	public String indexGET() {
+		
+		return "templateView/index";
+	}
+	
+	// http://localhost:8088/templateView/about
+	@GetMapping("/templateView/about")
+	public String aboutGET() {
+		
+		return "templateView/about";
+	}
+	// http://localhost:8088/templateView/contact
+	@GetMapping("/templateView/contact")
+	public String contactGET() {
+		
+		return "templateView/contact";
+	}
+	// http://localhost:8088/templateView/shop
+	@GetMapping("/templateView/shop")
+	public String shopGET() {
+		
+		return "templateView/shop";
+	}
+	// http://localhost:8088/templateView/shop-single
+	@GetMapping("/templateView/shop-single")
+	public String shopsingleGET() {
+		
+		return "templateView/shop-single";
+	}
+
+}
